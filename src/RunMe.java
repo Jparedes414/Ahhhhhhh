55637a65bcadbaffb2ba6e52c4d695dd11fd719f
yan the man <yteng575@student.fuhsd.org>
2022-02-01 10:34:03 -0800

    Webcam Works

--- src/RunMe.java
+++ src/RunMe.java
@@ -5,6 +5,7 @@ public class RunMe {
         // --== Load an image to filter ==--
         //DisplayWindow.showFor("images/6.jpg", 800, 600);
 
+
         // --== Determine your input interactively with menus ==--
         DisplayWindow.getInputInteractively(800,600);
     }
